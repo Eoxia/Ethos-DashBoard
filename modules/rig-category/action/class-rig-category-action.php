558cbd37e140bfc364cff4ad6b3b54e00f4b5562
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-07-11 10:56:34 +0200

    Regenerate all rigs txt in current category when edit it.

--- modules/rig-category/action/class-rig-category-action.php
+++ modules/rig-category/action/class-rig-category-action.php
@@ -25,6 +25,8 @@ class Rig_Category_Action {
 	 */
 	public function __construct() {
 		add_action( 'init', array( $this, 'callback_init' ) );
+
+		add_action( 'edited_terms', array( $this, 'callback_edit_terms'), 10, 2 );
 	}
 
 	/**
@@ -51,6 +53,39 @@ class Rig_Category_Action {
 		);
 
 		register_taxonomy( Rig_Category_Class::g()->taxonomy, array( 'rig' ), $args );
+	}
+
+	/**
+	 * This method regenerate all txt files from the current category;
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param integer $term_id  L'ID du term.
+	 * @param string  $taxonomy Le nom de la taxonomy.
+	 */
+	public function callback_edit_terms( $term_id, $taxonomy ) {
+		if ( Rig_Category_Class::g()->taxonomy === $taxonomy ) {
+			// Tous les rigs ayant cette catégorie.
+			// Regénéré le fichier TXT.
+			$rigs = get_posts( array(
+				'post_type'      => 'rig',
+				'posts_per_page' => -1,
+			) );
+
+			if ( ! empty( $rigs ) ) {
+				foreach ( $rigs as $rig ) {
+					$rig_id        = $rig->ID;
+					$category      = get_term( $term_id, Rig_Category_Class::g()->taxonomy );
+					$category->acf = Rig_Category_Class::g()->get_fields( $category->term_id );
+					$wallet        = Wallet_Class::g()->get_fields( $category->acf['wallet_id'] );
+					$rig           = Rig_Class::g()->get_fields( $rig->ID );
+
+					unset( $category->acf['wallet_id'] );
+
+					Rig_Class::g()->generate( $rig_id, $rig, $wallet, $category->acf );
+				}
+			}
+		}
 	}
 }
 
